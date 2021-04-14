23f8c1ecc400bdf66bd057ac720dc463f883be27
Greg Gompers <ggompers28@gmail.com>
2021-04-14 14:01:47 -0400

    Add - CNN from Udacity Computer Vision Expert

--- pytorch_exploration_1.py
+++ pytorch_exploration_1.py
@@ -425,3 +425,36 @@ with open("nn_tests_output.txt", "a") as output_file:
 # all these things, batch size, number of epochs, number of layers, have an effect on accuracy,
 # acc 96.9 after 20 epochs of FCNN with batch of 512 in 115 sec
 # acc 97.3 after 40 epochs of FCNN with batch 512, in 240 sec (after loading from 20 above)
+
+
+# Work from Udacity Computer Vision Expert
+# Basic CNN Architecture with one Conv layer and one pooling layer
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+
+    def __init__(self, weight):
+        super(Net, self).__init__()
+        # initializes the weights of the convolutional layer to be the weights of the 4 defined filters
+        k_height, k_width = weight.shape[2:]
+        # assumes there are 4 grayscale filters
+        self.conv = nn.Conv2d(1, 4, kernel_size=(k_height, k_width), bias=False)
+        self.conv.weight = torch.nn.Parameter(weight)
+        # define a pooling layer
+        self.pool = nn.MaxPool2d(4, 4)
+
+    def forward(self, x):
+        # calculates the output of a convolutional layer
+        # pre- and post-activation
+        conv_x = self.conv(x)
+        activated_x = F.relu(conv_x)
+
+        # applies pooling layer
+        pooled_x = self.pool(activated_x)
+
+        # returns all layers
+        return conv_x, activated_x, pooled_x
+
